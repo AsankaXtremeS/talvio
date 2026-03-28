36c7f6d0dfba8da97882b5738f81a7b0d76639bb
asanka <asankasampath200228@gmail.com>
2026-03-28 11:52:01 +0530

    feat: add jobPosts relation to EmployerProfile and update routes for employer job posts

---
+++ backend/prisma/migrations/20260328061236_job_post_added/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `department` on the `JobPost` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "JobPost" DROP COLUMN "department";
